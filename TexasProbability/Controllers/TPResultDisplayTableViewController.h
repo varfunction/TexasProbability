554d3b63e23199728b10b3d7d23772dbea2407db
ocean tang <405577829@qq.com>
2014-06-20 18:57:02 +0800

    基本功能完善

---
+++ TexasProbability/Controllers/TPResultDisplayTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TPResultDisplayTableViewController.h
+//  TexasProbability
+//
+//  Created by ocean tang on 14-6-20.
+//  Copyright (c) 2014年 ocean tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TPResultDisplayTableViewController : UITableViewController
+
+@end
